d26cb98f00db2eef4e41faa506b7a9772eb15a09
Shivani Surve <surveshivani99@gmail.com>
2026-02-28 13:33:26 +0530

    Add files via upload

---
+++ Control_Assignment/Control_Assignment_01/Control_Assignment_01/Forms/Emp_Details.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_Assignment_01
+{
+    public partial class frm_Emp_Details : Form
+    {
+        public frm_Emp_Details()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Emp_Details_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
